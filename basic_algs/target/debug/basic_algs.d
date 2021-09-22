e0ca452f229ad08a83f4b56e3596b597346cab4c
Gleb Oborin <Gleb.Oborin@jetbrains.com>
2021-09-23 02:08:13 +0300

    removed useless files

--- basic_algs/target/debug/basic_algs.d
+++
@@ -1,1 +0,0 @@
-/Users/Gleb.Oborin/Desktop/SPBU/rust/basic_algs/target/debug/basic_algs: /Users/Gleb.Oborin/Desktop/SPBU/rust/basic_algs/src/main.rs
--- basic_algs/target/debug/deps/basic_algs.d
+++
@@ -1,5 +0,0 @@
-/Users/Gleb.Oborin/Desktop/SPBU/rust/basic_algs/target/debug/deps/basic_algs: src/main.rs
-
-/Users/Gleb.Oborin/Desktop/SPBU/rust/basic_algs/target/debug/deps/basic_algs.d: src/main.rs
-
-src/main.rs:
